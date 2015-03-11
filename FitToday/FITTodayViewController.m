9dd01d481cfb1b140f9ca6258388d353d5b01d70
Dasmer Singh <dasmer.singh@venmo.com>
2015-03-11 13:50:09 -0400

    Create RelativeFitDataKit Framework

--- FitToday/FITTodayViewController.m
+++ FitToday/FITTodayViewController.m
@@ -1,20 +1,41 @@
 #import "FITTodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
 
+@import RelativeFitDataKit;
+
 @interface FITTodayViewController () <NCWidgetProviding>
 
 @property (weak, nonatomic) IBOutlet UILabel *stepsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *floorsLabel;
+@property (strong, nonatomic) FITPedometer *pedometer;
 
 @end
 
 @implementation FITTodayViewController
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    return  [super initWithCoder:aDecoder];
+}
+
+- (instancetype)init
+{
+    return [super init];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    __weak typeof (self) weakSelf = self;
+    self.pedometer = [[FITPedometer alloc] initWithDidUpdateBlock:^(FITPedometerData *pedometerData) {
+        __weak typeof (self) strongSelf = weakSelf;
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            strongSelf.stepsLabel.text = [@([pedometerData.numberOfStepsDelta integerValue]) stringValue];
+            strongSelf.distanceLabel.text = [@([pedometerData.numberOfMetersDelta integerValue]) stringValue];
+            strongSelf.floorsLabel.text = [@([pedometerData.numberOfFloorsDelta integerValue]) stringValue];
+        }];
+    }];
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler
