ccd1824f6506bd4799b047f8d0490daad4425418
Dasmer Singh <dasmer.singh@venmo.com>
2015-03-11 11:46:12 -0400

    Create Today Widget Target

---
+++ FitToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  FitToday
+//
+//  Created by Singh, Das on 3/11/15.
+//  Copyright (c) 2015 Columbia University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
