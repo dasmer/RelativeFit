077e9a14730364abad696fac038ba3760819411d
dasmer <dasmersingh@gmail.com>
2015-03-12 23:45:15 -0400

    Remove Header and rename bundle display name

--- RelativeFit WatchKit Extension/InterfaceController.h
+++ RelativeFit WatchKit Extension/InterfaceController.h
@@ -1,11 +1,3 @@
-//
-//  InterfaceController.h
-//  RelativeFit WatchKit Extension
-//
-//  Created by dasmer on 3/12/15.
-//  Copyright (c) 2015 Columbia University. All rights reserved.
-//
-
 #import <WatchKit/WatchKit.h>
 #import <Foundation/Foundation.h>
 
